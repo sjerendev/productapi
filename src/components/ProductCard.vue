ba562c6960d810df3276a56a297f5deb9d6b265c
sjeren1501 <groovepromo@gmail.com>
2023-12-20 08:46:24 +0100

    initial commit

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="product-card">
+    <h2>{{ product.name }}</h2>
+    <p><b>Märke</b> {{ product.brand }}</p>
+    <p><b>Storlek</b> {{ product.size }} {{ product.unit_abbr }}</p>
+    <p><b>Ursprung</b> {{ product.country_alpha2 }}</p>
+    <p><b>Pris</b> {{ product.average_price }} {{ product.currency }}</p>
+    <p><b>Klimat</b> {{ product.climate_score }}</p>
+    <p><b>Hälso</b> {{ product.health_score }}</p>
+    <p><b>Rättvisa</b> {{ product.fairness_score }}</p>
+  </div>
+</template>
+
+<style scoped>
+.product-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.product-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,79 @@
+<script setup>
+import ProductCard from '@/components/ProductCard.vue'
+import { ref } from 'vue'
+
+const products = ref([
+  {
+    id: 22797,
+    name: 'Ekol Ketchup Krav',
+    brand: 'FELIX',
+    size: '2500',
+    unit_abbr: 'g',
+    image_url:
+      'https://s3-eu-west-1.amazonaws.com/valstorage.consupedia.com/products.320x320/498/07310240084498.product.320x320.000.png',
+    country_alpha2: 'SE',
+    is_eco: 1,
+    ethic_id: 3,
+    for_you: 'CANT_TELL',
+    average_price: null,
+    currency: 'kr',
+    climate_score: 52,
+    water_score: null,
+    health_score: 35,
+    fairness_score: 75
+  },
+  {
+    id: 22794,
+    name: 'Krav Eko Ketchup Msms Sentomat',
+    brand: 'FELIX',
+    size: '2500',
+    unit_abbr: 'g',
+    image_url:
+      'https://s3-eu-west-1.amazonaws.com/valstorage.consupedia.com/products.320x320/399/07310240084399.product.320x320.000.png',
+    country_alpha2: 'SE',
+    is_eco: 1,
+    ethic_id: 3,
+    for_you: 'CANT_TELL',
+    average_price: null,
+    currency: 'kr',
+    climate_score: 52,
+    water_score: null,
+    health_score: 55,
+    fairness_score: 75
+  },
+  {
+    id: 2271151394,
+    name: 'Ketchup Ekologisk',
+    brand: 'Heinz',
+    size: '1015',
+    unit_abbr: 'g',
+    image_url:
+      'https://s3-eu-west-1.amazonaws.com/images.consupedia.com/products/08715700413341.jpg',
+    country_alpha2: 'NL',
+    is_eco: 1,
+    ethic_id: 3,
+    for_you: 'CANT_TELL',
+    average_price: 30,
+    currency: 'kr',
+    climate_score: 51,
+    water_score: null,
+    health_score: 35,
+    fairness_score: 74
+  }
+])
+</script>
+
+<template>
+  <h1>Products for good</h1>
+  <div class="products">
+    <ProductCard v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<style scoped>
+.products {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
